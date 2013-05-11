4f9d7205806275a48efe2e49ba57df61d3ad7982
Corey Leigh Latislaw <colabug@gmail.com>
2013-05-11 12:44:35 -0400

    Initial commit

---
+++ src/com/colabug/TardisNoFragments/TardisPreFragmentsActivity.java
@@ -0,0 +1,14 @@
+package com.colabug.TardisNoFragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TardisPreFragmentsActivity extends Activity
+{
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.main );
+    }
+}
