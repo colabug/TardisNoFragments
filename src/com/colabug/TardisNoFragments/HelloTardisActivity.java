bf48de035669bc8d2a2a0626a2ac2f29b246be2f
Corey Leigh Latislaw <colabug@gmail.com>
2013-05-30 18:35:52 -0400

    REFACTOR: Renamed for clarity and to match first lab; removed fluff

--- src/com/colabug/TardisNoFragments/HelloTardisActivity.java
+++ src/com/colabug/TardisNoFragments/HelloTardisActivity.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
-public class OutsideTardisActivity extends Activity
+public class HelloTardisActivity extends Activity
 {
     @Override
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.outside_tardis );
+        setContentView( R.layout.hello_tardis );
 
         View tardisImage = findViewById( R.id.tardis_image );
         tardisImage.setOnClickListener( new View.OnClickListener()
